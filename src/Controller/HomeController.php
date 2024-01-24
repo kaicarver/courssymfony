685f60412c47bb25b25185a0a7d091513e9f0fbd
Robert Carver <155527754+robertcarv@users.noreply.github.com>
2024-01-24 13:45:21 +0100

    premiers pas avec Symfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/pageTexte/{id?}', name: 'text_home')]
+    public function page1($id): Response
+    {
+        return new Response("Hello tout le monde " . $id);
+    }
+
+    #[Route('/site', name: 'site_home')]
+    public function site(): Response
+    {
+        return $this->redirect("https://symfony.com");
+    }
+
+    #[Route('/redirectHome', name: 'homeredirect_home')]
+    public function redirectHome(): Response
+    {
+        return $this->redirectToRoute("app_home");
+    }
+}
