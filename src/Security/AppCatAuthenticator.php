f61d8e13da94412626008ac2c48082f235836c46
Robert Carver <155527754+robertcarv@users.noreply.github.com>
2024-01-30 10:26:12 +0100

    valid redirect

--- src/Security/AppCatAuthenticator.php
+++ src/Security/AppCatAuthenticator.php
@@ -49,8 +49,8 @@ class AppCatAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('list_categorie'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
