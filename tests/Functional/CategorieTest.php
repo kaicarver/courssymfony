f13da3598d647b3d21fcb5a30774fa59a48bd6c5
Robert Carver <155527754+robertcarv@users.noreply.github.com>
2024-02-01 16:06:47 +0100

    test avec auth

--- tests/Functional/CategorieTest.php
+++ tests/Functional/CategorieTest.php
@@ -2,13 +2,33 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\Utilisateur;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CategorieTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testFormCategorieConnected(): void
+    {
+        $client = static::createClient();
+        $urlGenerator = $client->getContainer()->get('router');
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+        $user = $entityManager->find(Utilisateur::class, 17);
+        $client->loginUser($user);
+
+        $crawler = $client->request(Request::METHOD_GET, $urlGenerator->generate('add_categorie'));
+
+        $form = $crawler->filter('form[name=categorie]')->form([
+            'categorie[nomcat]' => 'Catégorie 1',
+            'categorie[description]' => 'description de la catégorie 1',
+        ]);
+        $client->submit($form);
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+        $client->followRedirect();
+        $this->assertRouteSame('list_categorie');
+    }
+    public function testFormCategorie(): void
     {
         $client = static::createClient();
         $urlGenerator = $client->getContainer()->get('router');
